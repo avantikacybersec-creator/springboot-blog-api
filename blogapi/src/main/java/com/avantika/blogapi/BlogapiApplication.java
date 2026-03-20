1e0f0be07232d7ba2c47db563c785090197108bd
Avantika Tagde <avantikacybersec@gmail.com>
2026-03-20 17:30:52 +0530

    fix project structure

--- blogapi/src/main/java/com/avantika/blogapi/BlogapiApplication.java
+++ blogapi/src/main/java/com/avantika/blogapi/BlogapiApplication.java
@@ -1,12 +1,18 @@
 package com.avantika.blogapi;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.avantika.blogapi.repository")
+@EntityScan(basePackages = "com.avantika.blogapi.entity")
 public class BlogapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BlogapiApplication.class, args);
 	}
 
--- blogapi/src/main/java/com/avantika/blogapi/repository/CommentRepository.java
+++ blogapi/src/main/java/com/avantika/blogapi/repository/CommentRepository.java
@@ -3,9 +3,10 @@ package com.avantika.blogapi.repository;
 
 import com.avantika.blogapi.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     List<Comment> findByPostId(Long postId);
