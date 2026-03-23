e93fc1fb58cf448dedacc423bb674c61600635d3
Avantika Tagde <avantikacybersec@gmail.com>
2026-03-23 12:20:07 +0530

    fix security2 completely

--- src/main/java/com/avantika/blogapi/controller/HomeController.java
+++ src/main/java/com/avantika/blogapi/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.avantika.blogapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
 
     @GetMapping("/")
